75de0c1101c14fcffe4c9a991be30c965b261a89
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 12:51:44 -0400

    Added color, upgraded Quartus

--- MUX4_32.v
+++ MUX4_32.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Thu May 12 08:30:35 2022"
+// CREATED		"Tue Aug 15 12:49:51 2023"
 
 module MUX4_32(
 	A,
